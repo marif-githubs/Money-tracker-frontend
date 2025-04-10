0b568ec7ccaf301e2086ff34095efedcafc23dbc
Syed Mohammed Miftahuddin Marif <syedmarif.professional@gmail.com>
2025-04-10 20:23:36 +0530

    link import fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react"
 import axios from "axios";
+import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
