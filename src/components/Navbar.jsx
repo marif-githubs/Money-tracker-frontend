325deef159e92b014d3b1ea7ea01889944f77349
Syed Mohammed Miftahuddin Marif <syedmarif.professional@gmail.com>
2025-04-10 22:05:11 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,32 +13,10 @@ export const Navbar = () => {
         <div className="flex-1">
           <a className="select-none text-xl">Money Tracker</a>
         </div>
-        {/*<div className="flex gap-2">
-           <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" /> */}
-          {/* <div className="dropdown dropdown-end"> */}
-         {/* <div tabIndex={0} role="button" className="btn btn-ghost b avatar">
-            <div className="w-10 rounded-full border-4 border-double">
-              <img
-                alt="Navbar"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-            </div>
-          </div>*/}
+        
           <button className="btn btn-soft btn-primary" onClick={() => handleLogout()}> Log out</button>
 
-          {/* <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li><a>Settings</a></li>
-              <li><a>Logout</a></li>
-            </ul> */}
-          {/* </div> 
-        </div>*/}
+          
       </div>
     </>
   )
