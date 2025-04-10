e226e52062b339b789dd7682a44c626311c19346
Syed Mohammed Miftahuddin Marif <syedmarif.professional@gmail.com>
2025-04-10 12:04:19 -0600

    Update Expenditure.jsx

--- src/pages/Expenditure.jsx
+++ src/pages/Expenditure.jsx
@@ -17,7 +17,7 @@ export const Expenditure = () => {
 
                     <p className="p-4 pb-2 text-lg text-center tracking-wide">Expenditures</p>
 
-                    {loading ? <Loading /> : expList === null ? <Message /> : expList.map(item => <List title={item.title} amount={item.amount} description={item.description} id={item.t_id} type={"expense"}></List>)}
+                    {loading ? <Loading /> : expList.length === 0 ? <Message/> : expList.map(item => <List title={item.title} amount={item.amount} description={item.description} id={item.t_id} type={"expense"}></List>)}
 
                 </ul>
             </div>
