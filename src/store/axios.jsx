1691c5e90dd188dec12377ed394944501d728de2
Syed Mohammed Miftahuddin Marif <syedmarif.professional@gmail.com>
2025-04-12 11:15:22 -0600

    Update axios.jsx

--- src/store/axios.jsx
+++ src/store/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:10000/",
+    baseURL: "https://money-tracker-backend-8trv.onrender.com/",
     headers: { "Content-Type": "application/json" },
 });
 
